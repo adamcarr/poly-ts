aa1c014f610df5281e8ef70c89c1f4ae3eacc90f
Adam Carr <adam.carr@gmail.com>
2015-06-26 23:26:54 -0700

    initial commit

---
+++ app/elements/proto-element/proto-element.js
@@ -0,0 +1,7 @@
+Polymer({
+    is: "proto-element",
+    // add a callback to the element's prototype
+    ready: function() {
+        this.textContent = "I'm a proto-element. Check out my prototype!"
+    }
+});
